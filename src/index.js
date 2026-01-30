2f8683121806127e5a1152a23c280304736c7465
suldozzang <115049092+suldozzang@users.noreply.github.com>
2026-01-30 13:38:37 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SolarSystem from './solar-system';
+import SolarSystem from './solar-system'; // 경로 수정됨
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
